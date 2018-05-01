3716ff6e5cf562bd4a36b8ebceda7ab99fd44e74
Kyosuke Sugiura <kyosuke.sugiura@keio.jp>
2018-05-01 15:59:05 +0900

    fix: parse command result

--- executecommand.js
+++ executecommand.js
@@ -2,9 +2,12 @@ const cmd = require('node-cmd');
 const  shellParser = require('node-shell-parser');
 
 cmd.get(
-  '/Users/Kyosuke/GitHub/iPOPDemo/fakenetstat',
+  // 'netstat -Ieth0',
+  './fakenetstat',
   function(err, data, stderr) {
       console.log(data)
-      console.log(shellParser(data, {skipLines: 1}))
+
+      console.log(data.split('\n')[2].split(' '))
+      // console.log(shellParser(data, {skipLines: 1}))
   }
 );
