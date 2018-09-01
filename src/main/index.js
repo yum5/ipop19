3365843b412444ecdd3bf847872ece850da1c95a
Kyosuke Sugiura <kyosuke.sugiura@keio.jp>
2018-09-01 16:24:12 +0900

    remove: old lines

--- src/main/index.js
+++ src/main/index.js
@@ -29,6 +29,9 @@ function createWindow () {
     // webPreferences: {
     //   nodeIntegration: false
     // }
+    webPreferences: {
+      nativeWindowOpen: true
+    }
   });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
@@ -59,184 +62,11 @@ app.on('activate', () => {
   }
 })
 
-const getPlatform = () => {
-  if (process.platform === 'linux') {
-    if (os.release().includes('el6')) {
-      return PLATFORM.centos6
-    } else if (os.release().includes('el7')) {
-      return PLATFORM.centos7
-    } else {
-      throw 'Unsupported Platform'
-    }
-  } else if (process.platform === 'darwin') {
-    return PLATFORM.darwin
-  } else {
-    throw 'Unsupported Platform'
-  }
-}
-
-// const getCommand = nic => {
-//   const platform = getPlatform()
-//
-//   switch (platform) {
-//     case PLATFORM.centos6:
-//       return `netstat -I${nic}`;
-//     case PLATFORM.centos7:
-//       return `ip -s link show dev ${nic}`;
-//     case PLATFORM.darwin:
-//       return `netstat -I ${nic}`;
-//   }
-// }
-//
-// const parseCommandResult = stdout => {
-//   const platform = getPlatform()
-//
-//   switch (platform) {
-//     case PLATFORM.centos6:
-//     {
-//       const result = stdout.split('\n')[2].split(/\s+/);
-//       return {
-//         rx: parseInt(result[3]),
-//         tx: parseInt(result[7])
-//       }
-//     }
-//     case PLATFORM.centos7:
-//     {
-//       const lines = stdout.split('\n');
-//       const lineRx = lines[3].split(/\s+/)
-//       const lineTx = lines[5].split(/\s+/)
-//
-//       return {
-//         rx: parseInt(lineRx[2]),
-//         tx: parseInt(lineTx[2])
-//       }
-//     }
-//     case PLATFORM.darwin:
-//     {
-//       const result = stdout.split('\n')[1].split(/\s+/);
-//
-//       return {
-//         rx: parseInt(result[4]),
-//         tx: parseInt(result[6])
-//       }
-//     }
-//   }
-// }
-
-const getPacketCount = (nic) => {
-  const platform = getPlatform()
-
-  switch (platform) {
-    case PLATFORM.centos6:
-    {
-      const parser = stdout => {
-        const result = stdout.split('\n')[2].split(/\s+/);
-        return {
-          rx: parseInt(result[3]),
-          tx: parseInt(result[7])
-        }
-      }
-
-      return {
-        command: `netstat -I${nic}`,
-        parser: parser
-      }
-    }
-    case PLATFORM.centos7:
-    {
-      const parser = stdout => {
-        const lines = stdout.split('\n');
-        const lineRx = lines[3].split(/\s+/)
-        const lineTx = lines[5].split(/\s+/)
-
-        return {
-          rx: parseInt(lineRx[2]),
-          tx: parseInt(lineTx[2])
-        }
-      }
-
-      return {
-        command: `ip -s link show dev ${nic}`,
-        parser: parser
-      }
-    }
-    case PLATFORM.darwin:
-    {
-      const parser = stdout => {
-        const result = stdout.split('\n')[1].split(/\s+/);
-
-        return {
-          rx: parseInt(result[4]),
-          tx: parseInt(result[6])
-        }
-      }
-
-      return {
-        command: `netstat -I ${nic}`,
-        parser: parser
-      }
-    }
-  }
-}
-
-// const getInterfaces = () => {
-//   const platform = getPlatform()
-//
-//   switch (platform) {
-//     case PLATFORM.centos6:
-//     {
-//       return {
-//         command: `netstat -i | tail -n +3`,
-//         parser: stdout => stdout.split('\n').map(v => v.split(/\s+/)[0]).filter(v => v)
-//       }
-//     }
-//     case PLATFORM.centos7:
-//     {
-//       return {
-//         command: `ip link show | grep -oE '^\[0-9]\+:\[ ]\+(.\+):'`,
-//         parser: stdout => stdout.split('\n').map(v => v.split(/\s+/)[1]).filter(v => v).map(v => v.replace(/:/, ''))
-//       }
-//     }
-//     case PLATFORM.darwin:
-//     {
-//       return {
-//         command: `networksetup -listallhardwareports | grep Device`,
-//         parser: stdout => stdout.split('\n').map(v => v.split(/\s+/)[1]).filter(v => v)
-//       }
-//     }
-//   }
-// }
-
-
 const settings = {
   interval: 1000,
   interfaces: [],
   selectedInterface: '',
 }
-
-ipcMain.on('settings_changed', function(event, newSetting) {
-  settings.interval = newSetting.interval;
-  settings.selectedInterface = newSetting.selectedInterface;
-});
-
-ipcMain.on('request_settings', function(event) {
-  // const { command, parser } = getInterfaces()
-  // cmd.get(command,
-  //   function(err, data, stderr) {
-  //     const list = parser(data);
-  //     settings.interfaces = list;
-  //     settings.selectedInterface = list[0];
-  //
-  //     if (mainWindow) {
-  //       mainWindow.webContents.send('receive_settings', {
-  //         interval: settings.interval,
-  //         interfaces: settings.interfaces,
-  //         selectedInterface: settings.selectedInterface
-  //       });
-  //     }
-  //   }
-  // );
-});
 
 const store = configureStore({}, 'main');
 setTimeout(() => {
@@ -249,24 +79,6 @@ const timer = new NanoTimer();
 
 const main = () => {
   timer.setTimeout(main, '', `${settings.interval}m`);
-
-  if (settings.selectedInterface !== '') {
-    const { command, parser } = getPacketCount(settings.selectedInterface)
-    cmd.get(command,
-      function(err, data, stderr) {
-        const result = parser(data);
-
-        if (mainWindow) {
-          mainWindow.webContents.send('packet_received', {
-            timestamp: new Date(),
-            rx: result.rx,
-            tx: result.tx
-          });
-        }
-
-      }
-    );
-  }
   // store.dispatch(packetCount('en0'));
 }
 
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -11,159 +11,37 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import React from 'react';
 import { render } from 'react-dom';
+// const app = new Vue({
+//   el: '#vue-root',
+//   data: {
+//     duration: 20000, // graph duration
+//     interval: 1000, // sampling interval
+//     interfaces: [],
+//     selectedInterface: ''
+//   },
+//   watch: {
+//     duration: _.debounce(function() {
+//       config.options.plugins.streaming.duration = this.duration;
+//       myBar.update();
+//     }, 100),
+//     interval: _.debounce(function() {
+//       ipcRenderer.send('settings_changed', {
+//         interval: this.interval,
+//         selectedInterface: this.selectedInterface
+//       });
+//     }, 100),
+//     selectedInterface: _.debounce(function() {
+//       ipcRenderer.send('settings_changed', {
+//         interval: this.interval,
+//         selectedInterface: this.selectedInterface
+//       });
+//     }, 100)
+//   }
+// });
 
-const orange = 'rgb(255, 159, 64)';
-const blue = 'rgb(64, 159, 255)';
-
-const config = {
-  type: 'line',
-  data: {
-    labels: [],
-    datasets: [{
-      key: 'rx',
-      label: 'Rx Packets',
-      backgroundColor: Chart.helpers.color(orange).alpha(0.5).rgbString(),
-      borderColor: orange,
-      borderWidth: 1,
-      data: []
-    },{
-      key: 'tx',
-      label: 'Tx Packets',
-      backgroundColor: Chart.helpers.color(blue).alpha(0.5).rgbString(),
-      borderColor: blue,
-      borderWidth: 1,
-      data: []
-    }]
-  },
-  options: {
-    responsive: true,
-    title: {
-      display: true,
-      text: 'Packet Monitor'
-    },
-    scales: {
-      xAxes: [{
-        type: 'realtime',
-        display: true
-      }],
-      yAxes: [{
-        type: 'linear',
-        display: true,
-        scaleLabel: {
-          display: true,
-          labelString: '# of packets sent / received per milliseconds'
-        }
-      }]
-    },
-    tooltips: {
-      mode: 'nearest',
-      intersect: false
-    },
-    hover: {
-      mode: 'nearest',
-      intersect: false
-    },
-    plugins: {
-      streaming: {
-        duration: 20000,
-        refresh: 1000,
-        delay: 1000
-      }
-    }
-  }
-};
-
-const ctx = document.getElementById('canvas').getContext('2d');
-const myBar = new Chart(ctx, config);
-
-const store = [];
-
-ipcRenderer.on('packet_received', function(event, data) {
-  const lastEntry = store.slice(-1)[0];
-  let nextEntry;
-
-  if (lastEntry) {
-    const timeInterval = moment(data.timestamp) - moment(lastEntry.timestamp);
-    const rxDelta = data.rx - lastEntry.rx;
-    const txDelta = data.tx - lastEntry.tx;
-
-    nextEntry = {
-      timestamp: data.timestamp,
-      rx: data.rx,
-      tx: data.tx,
-      rxDelta: rxDelta,
-      txDelta: txDelta,
-      timeInterval: timeInterval,
-      rxDeltaPerSec: rxDelta / timeInterval,
-      txDeltaPerSec: txDelta / timeInterval
-    };
-  } else {
-    nextEntry = {
-      timestamp: data.timestamp,
-      rx: data.rx,
-      tx: data.tx,
-      rxDelta: 0,
-      txDelta: 0,
-      timeInterval: 0,
-      rxDeltaPerSec: 0,
-      txDeltaPerSec: 0
-    };
-  }
-  store.push(nextEntry);
-
-  config.data.datasets.forEach(function(dataset) {
-    if (dataset.key === 'rx') {
-      dataset.data.push({
-        x: nextEntry.timestamp,
-        y: nextEntry.rxDeltaPerSec
-      });
-    } else if (dataset.key === 'tx') {
-      dataset.data.push({
-        x: nextEntry.timestamp,
-        y: nextEntry.txDeltaPerSec
-      });
-    }
-  });
-});
-
-const app = new Vue({
-  el: '#vue-root',
-  data: {
-    duration: 20000, // graph duration
-    interval: 1000, // sampling interval
-    interfaces: [],
-    selectedInterface: ''
-  },
-  watch: {
-    duration: _.debounce(function() {
-      config.options.plugins.streaming.duration = this.duration;
-      myBar.update();
-    }, 100),
-    interval: _.debounce(function() {
-      ipcRenderer.send('settings_changed', {
-        interval: this.interval,
-        selectedInterface: this.selectedInterface
-      });
-    }, 100),
-    selectedInterface: _.debounce(function() {
-      ipcRenderer.send('settings_changed', {
-        interval: this.interval,
-        selectedInterface: this.selectedInterface
-      });
-    }, 100)
-  }
-});
-
-ipcRenderer.send('request_settings');
-ipcRenderer.on('receive_settings', function(event, data) {
-  app.interval = data.interval;
-  app.interfaces = data.interfaces;
-  app.selectedInterface = data.selectedInterface;
-});
-
-const _store = configureStore({}, 'renderer');
+const store = configureStore({}, 'renderer');
 render(
-  <Provider store={_store}>
+  <Provider store={store}>
     <div>
       <App />
     </div>
